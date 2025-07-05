95118f7230174ad88596e3c50a027fed678a8246
Reim-developer <contact.kaxtr@gmail.com>
2025-07-05 16:32:41 +0700

    Auto generated by `ncc`

--- dist/index.js
+++ dist/index.js
@@ -26410,7 +26410,7 @@ class ActionManager {
         this.downloadManager = new downloadManager_1.default();
         this.binaryManager = new binaryManger_1.default();
     }
-    run() {
+    async run() {
         if (!this.configManager.IsValidConfigPath()) {
             const wrongConfigPath = this.configManager.configPath;
             actionCore.setFailed(`Could not find your TOML config: ${wrongConfigPath}`);
@@ -26419,12 +26419,10 @@ class ActionManager {
         const configPath = this.configManager.GetConfigPath();
         actionCore.info(`Found your TOML configuration: ${configPath}`);
         actionCore.info("Started download GoHook archived:");
-        (async () => {
-            let lastestVersionTag = await this.binaryManager.GetLatestVersion();
-            if (lastestVersionTag != null) {
-                await this.downloadManager.DownloadGoHookArchived(lastestVersionTag);
-            }
-        })();
+        let lastestVersionTag = await this.binaryManager.GetLatestVersion();
+        if (lastestVersionTag != null) {
+            await this.downloadManager.DownloadGoHookArchived(lastestVersionTag);
+        }
     }
 }
 exports["default"] = ActionManager;
@@ -26816,7 +26814,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const actionManager_1 = __importDefault(__nccwpck_require__(3190));
-void new actionManager_1.default().run();
+(async function () {
+    let actionManager = new actionManager_1.default();
+    await actionManager.run();
+})();
 
 
 /***/ }),
