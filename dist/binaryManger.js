00a307edcf2bdc7db966c3e0b699022cf1f5d984
Reim-developer <contact.kaxtr@gmail.com>
2025-07-05 15:50:01 +0700

    Enhance error handling in GetSpecifyVersion and GetLatestVersion methods of BinaryManager

--- dist/binaryManger.js
+++ dist/binaryManger.js
@@ -39,6 +39,46 @@ class BinaryManager {
     constructor() {
         this.version = coreActions.getInput("use-cli-version") || "latest";
     }
+    async GetSpecifyVersion(versionTag) {
+        const RESULT = new Promise((resolv) => {
+            const REQUEST_OPTIONS = {
+                hostname: BinaryManager.API_HOST_NAME,
+                path: `${BinaryManager.RELEASE_PAGE_URL}/tags/${versionTag}`,
+                method: "GET",
+                headers: {
+                    "User-Agent": "GoHookActions"
+                }
+            };
+            const REQUEST = https.request(REQUEST_OPTIONS, function (response) {
+                let responseBody = "";
+                response.on("data", (chunk) => responseBody += chunk);
+                response.on("end", function () {
+                    const JSON_DATA = JSON.parse(responseBody);
+                    const BINARY_VERSION = JSON_DATA.tag_name;
+                    if (!BINARY_VERSION) {
+                        const FAILED_STATUS = response.statusCode;
+                        const FAILED_MESSAGE = `GitHub API did not return a valid tag name for version ${versionTag} release.`;
+                        if (FAILED_STATUS == undefined) {
+                            coreActions.setFailed(FAILED_MESSAGE);
+                            coreActions.setFailed("Could not get the response status. Maybe it's undefined.");
+                            return;
+                        }
+                        coreActions.setFailed(FAILED_MESSAGE);
+                        coreActions.setFailed(`Response status code: ${FAILED_STATUS}`);
+                        return resolv(null);
+                    }
+                    resolv(BINARY_VERSION);
+                });
+            });
+            REQUEST.on("error", function (error) {
+                coreActions.setFailed("Request to GitHub API is failed:");
+                coreActions.setFailed(`Full error message:  ${error.message}`);
+                return resolv(null);
+            });
+            REQUEST.end();
+        });
+        return RESULT;
+    }
     async GetLatestVersion() {
         const RESULT = new Promise((resolv) => {
             const REQUEST_OPTIONS = {
@@ -56,14 +96,23 @@ class BinaryManager {
                     const JSON_DATA = JSON.parse(responseBody);
                     const BINARY_VERSION = JSON_DATA.tag_name;
                     if (!BINARY_VERSION) {
-                        coreActions.setFailed("GitHub API did not return a valid tag name for latest release.");
+                        const FAILED_STATUS = response.statusCode;
+                        const FAILED_MESSAGE = "GitHub API did not return a valid tag name for latest release.";
+                        if (FAILED_STATUS == undefined) {
+                            coreActions.setFailed(FAILED_MESSAGE);
+                            coreActions.setFailed("Could not get the response status. Maybe it's undefined.");
+                            return;
+                        }
+                        coreActions.setFailed(FAILED_MESSAGE);
+                        coreActions.setFailed(`Response status code: ${FAILED_STATUS}`);
                         return resolv(null);
                     }
                     resolv(BINARY_VERSION);
                 });
             });
             REQUEST.on("error", function (error) {
-                coreActions.setFailed(`Request to GitHub API is failed: ${error.message}`);
+                coreActions.setFailed("Request to GitHub API is failed:");
+                coreActions.setFailed(`Full error message:  ${error.message}`);
                 return resolv(null);
             });
             REQUEST.end();
