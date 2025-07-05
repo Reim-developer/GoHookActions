bc61b69ffe02e2002556fca3a2b15b798c09b69b
Reim-developer <contact.kaxtr@gmail.com>
2025-07-05 16:18:08 +0700

    Fix anonymous function is not working in download step

--- dist/actionManager.js
+++ dist/actionManager.js
@@ -60,7 +60,7 @@ class ActionManager {
             if (lastestVersionTag != null) {
                 await this.downloadManager.DownloadGoHookArchived(lastestVersionTag);
             }
-        });
+        })();
     }
 }
 exports.default = ActionManager;
